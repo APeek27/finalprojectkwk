5468187bb3d14a7c629113a4f1b472137b8f5db4
Abigail Peek <peekabigail@gmail.com>
2022-06-16 11:29:54 -0400

    segways and buttons

--- finalprojectkwk/HomeViewController.swift
+++ finalprojectkwk/HomeViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBAction func motivationButton(_ sender: UIButton) {
+    }
+    @IBAction func videosButton(_ sender: UIButton) {
+    }
+    @IBAction func knowledgeButton(_ sender: UIButton) {
+    }
+    @IBAction func linksButton(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
