4a9ec4ed10b6b7f84b81468e318e1899c944719d
Abigail Peek <peekabigail@gmail.com>
2022-06-16 11:31:18 -0400

    segways

--- finalprojectkwk/HomeViewController.swift
+++ finalprojectkwk/HomeViewController.swift
@@ -19,6 +19,7 @@ class HomeViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
